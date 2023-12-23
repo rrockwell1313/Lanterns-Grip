a2148108a0a6fdee3e0736c39639e43b9709a46c
bubbadubc <brandon.channel@gmail.com>
2023-12-22 22:56:06 -0600

    Messed with Film Grain
    Added some Post Processing to get an Idea

--- Assets/Scripts/areaLightRangeRNG.cs
+++ Assets/Scripts/areaLightRangeRNG.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class areaLightRangeRNG : MonoBehaviour
 {
+    public Light areaLight;
+    public float range;
+    public float startingRange;
+    float oldRange;
+    public float frequency; //number of frames between updates
+    float frequencyReset;
+    public float time;      //time between adjustments
+    public float flicker;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        range = areaLight.range;
+        startingRange = range;
+        frequencyReset = frequency;
+        SetNewLightRange();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (frequency < frequencyReset) { frequency++; }
+        if (frequency >= frequencyReset) { SetNewLightRange();}
+    }
+
+    void SetNewLightRange()
+    {
+        oldRange = range;
+        range = Random.Range(startingRange - flicker, startingRange + flicker);
+        areaLight.range = range;
+        frequency = 0;
     }
 }
