11a171c8a4cf95674c1305c1a7cd5938e8f0ae54
bubbadubc <brandon.channel@gmail.com>
2023-12-22 23:38:55 -0600

    Imported packages
    Added cinemachine and the input manager

--- Assets/Scripts/areaLightRangeRNG.cs
+++ Assets/Scripts/areaLightRangeRNG.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
 
 public class areaLightRangeRNG : MonoBehaviour
 {
@@ -13,14 +16,18 @@ public class areaLightRangeRNG : MonoBehaviour
     float frequencyReset;
     public float time;      //time between adjustments
     public float flicker;
+    public Camera mainCamera;
+    public PostProcessVolume volume;
+    Bloom bloom;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        range = areaLight.range;
         startingRange = range;
         frequencyReset = frequency;
         SetNewLightRange();
+
     }
 
     // Update is called once per frame
@@ -32,6 +39,7 @@ public class areaLightRangeRNG : MonoBehaviour
 
     void SetNewLightRange()
     {
+        
         oldRange = range;
         range = Random.Range(startingRange - flicker, startingRange + flicker);
         areaLight.range = range;
