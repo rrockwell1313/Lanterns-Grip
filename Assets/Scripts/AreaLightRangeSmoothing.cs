694714b238d26ead11313154d8f99cb49c88760d
rrockwell1313 <122168611+rrockwell1313@users.noreply.github.com>
2023-12-24 15:02:49 -0600

    adjustments to lighting

---
+++ Assets/Scripts/AreaLightRangeSmoothing.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing; // Post-processing stack reference.
+
+// Class to create a flickering light effect, resembling a flame or unstable light source.
+#region NOTES ON FLICKER
+/*
+ * This version of the flicker uses perlin noise, and math to scale UP and DOWN in increments over time following a curve to simulate the flame flicker.
+ * We can adjust this pretty heavily to get it right based on the lantern, and we can even create profiles for different types of lanterns for more stability
+ * or maybe this one has glass panes protecting the flame so its less. Or maybe we increase the flickering based on movement to simulate wind hitting it.
+ * Or maybe a gust of wind comes to blow the wick out etc.
+ */
+#endregion
+public class AreaLightRangeSmoothing : MonoBehaviour
+{
+    public Light areaLight;
+    public AnimationCurve flickerCurve; // A curve to control the flicker intensity
+    public float minRange;
+    public float maxRange;
+    public float flickerDuration; // Duration of one flicker to reach the target intensity
+    public float flickerIntensity; // The maximum deviation added to the light's range due to flicker
+    public PostProcessVolume volume; // Reference to the post-process volume for potential effects 
+    private Bloom bloom;               // Reference to a Bloom effect if needed 
+    public float bloomOffset;
+
+
+    private float targetRange;//target range may not be relevant
+    private Coroutine flickerCoroutine;//error listed is false, its used in the start.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (volume.profile.HasSettings<Bloom>())
+        {
+            Debug.Log("bloom exists");
+            bloom = volume.profile.GetSetting<Bloom>();
+        }
+        // Set initial range
+        areaLight.range = minRange;
+        // Start the flickering effect
+        flickerCoroutine = StartCoroutine(FlickerLight());
+    }
+
+    IEnumerator FlickerLight()
+    {
+        // The time at which the flicker started.
+        float startTime = Time.time;
+
+        while (true)
+        {
+            // Calculate the time elapsed since the flicker started.
+            float timeElapsed = Time.time - startTime;
+
+            // Use a sine wave to oscillate the range value smoothly over time.
+            // Mathf.PingPong will smoothly transition the value between the minRange and maxRange.
+            float pingPong = Mathf.PingPong(timeElapsed * flickerDuration, maxRange - minRange);
+
+            // Apply a random factor to the ping pong value for variability, simulating a flame.
+            // Perlin noise can be used here for smoother random transitions.
+            float noise = Mathf.PerlinNoise(timeElapsed, 0f) * flickerIntensity;
+
+            // Set the new light range, combining the ping pong and noise values.
+            // Adjust the minRange accordingly to ensure it stays within the desired limits.
+            areaLight.range = minRange + pingPong + noise;
+            if (bloom)
+            {
+                bloom.intensity.value = (minRange + bloomOffset) + pingPong + noise;
+            }
+
+
+
+            // Wait until the next frame to update again.
+            yield return null;
+        }
+    }
+
+}
--- Assets/Scripts/areaLightRangeRNG.cs
+++ Assets/Scripts/areaLightRangeRNG.cs
@@ -1,48 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
+using Unity.VisualScripting; 
+using UnityEditor;           
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering.PostProcessing; // Post-processing stack reference.
 
-public class areaLightRangeRNG : MonoBehaviour
+// Class to create a flickering light effect, resembling a flame or unstable light source.
+#region NOTES ON FLICKER
+/*
+ * One of the issues we may run into with this script, is how post processing effects come from other light sources. For example
+ * we may have another torch, with its own flicker settings. Perhaps we remove the bloom flicker and post processing effect if there
+ * is any kind of issues with this, or find a way to create a list of light objects within range that may effect the bloom sensitivity as this is
+ * a camera effect, and not really a on scene effect like the lighting itself is. Something worth testing to see if this is the case.
+ */
+#endregion
+public class AreaLightRangeRNG : MonoBehaviour
 {
-    public Light areaLight;
-    public float range;
-    public float startingRange;
-    float oldRange;
-    public float frequency; //number of frames between updates
-    float frequencyReset;
-    public float time;      //time between adjustments
-    public float flicker;
-    public Camera mainCamera;
-    public PostProcessVolume volume;
-    Bloom bloom;
-
+    public Light areaLight;    // Reference to the Light component intended to flicker.
+    public float range;        // Current range of the area light.
+    public float startingRange; // Starting range of the area light, to reset or use as a base for calculations.
+    float oldRange;            // Previous range value for comparison or other logic (unused in current script for some reason? Need to debug.).
+    public float frequency;    // Number of frames between light range updates.
+    float frequencyReset;      // The initial value of frequency to reset after each update.
+    public float time;         // Time between adjustments (unused in current script. I dont forsee this being an issue to use.).
+    public float flicker;      // Amount of range variation to simulate flickering.
+    public Camera mainCamera;  // Reference to the main camera (unused in current script. Will not need as we have access to the volume).
+    public PostProcessVolume volume; // Reference to the post-process volume for potential effects 
+    private Bloom bloom;               // Reference to a Bloom effect if needed 
+    public float bloomMinimum;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (volume.profile.HasSettings<Bloom>())
+        {
+            Debug.Log("bloom exists");
+            bloom = volume.profile.GetSetting<Bloom>();
+        }
         startingRange = range;
         frequencyReset = frequency;
-        SetNewLightRange();
-
+        SetNewLightRange();//immediatley set new range on start.
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (frequency < frequencyReset) { frequency++; }
-        if (frequency >= frequencyReset) { SetNewLightRange();}
+        // If frequency counter has reached the threshold, set a new light range.
+        if (frequency >= frequencyReset) { SetNewLightRange(); }
     }
 
+    // Sets the light's range to a new random value within the flicker boundaries and resets the frequency counter.
     void SetNewLightRange()
     {
-        
-        oldRange = range;
+        oldRange = range;//referenced in variables, but not effecting. Weird.
+        float newBloomIntensity = Random.Range((startingRange + bloomMinimum) - flicker, startingRange + flicker);
+        if (bloom != null)
+        {
+            bloom.intensity.value = newBloomIntensity;
+        }        
         range = Random.Range(startingRange - flicker, startingRange + flicker);
         areaLight.range = range;
         frequency = 0;
     }
 }
+
+
+
