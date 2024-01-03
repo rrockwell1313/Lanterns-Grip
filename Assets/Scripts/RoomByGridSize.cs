9c6e3bf9998797bb3b24a51ac9241a42a5e11716
bubbadubc <brandon.channel@gmail.com>
2024-01-03 08:34:50 -0600

    RoomGenerator #2

---
+++ Assets/Scripts/RoomByGridSize.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomByGridSize : MonoBehaviour
+{
+    public GameObject test;
+    private GameObject wallTilePrefab;
+    private GameObject room;
+    public int roomWidth, roomHeight, roomDepth;
+    private float calcRoomWidth, calcRoomHeight, calcRoomDepth;
+    public float prefabWidth, prefabHeight;
+    public List<GameObject> wallPrefabList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateRoom();
+    }
+    void CreateRoom()
+    {
+        calcRoomDepth = roomDepth;
+        calcRoomHeight = roomHeight;
+        calcRoomWidth = roomWidth;
+        room = new GameObject("Room"); //create a new game object called Room
+        ChooseRandomPrefab();
+        GenerateWalls();
+    }
+    void ChooseRandomPrefab()
+    {
+        //choose a random prefab from a prebuilt list of prefabs
+        wallTilePrefab = wallPrefabList[Random.Range(0, wallPrefabList.Count)];
+    }
+    void GenerateWalls()
+    {
+        //each wall will have its own orientation, its own offset
+        NorthWall();
+        SouthWall();
+        EastWall();
+        WestWall();
+    }
+
+    void NorthWall()
+    {
+        float xOffset = 0;
+        float yOffset = 0;
+
+        for (int x = 0; x < calcRoomWidth; x++)
+        {
+            for (int y = 0; y < calcRoomHeight; y++)
+            {
+                Vector3 position = new Vector3((x * prefabWidth) + xOffset, (y * prefabHeight) + yOffset, calcRoomDepth / 2 * prefabWidth);
+                GameObject currentPrefab = Instantiate(wallTilePrefab, position, Quaternion.Euler(0, 180, 0), room.transform); //spawns the prefab facing inward
+                currentPrefab.name = $"Prefab_NorthWall{x}_{y}";
+                wallTilePrefab = wallPrefabList[Random.Range(0, wallPrefabList.Count)];
+            }
+        }
+    }
+
+    void SouthWall()
+    {
+        //for south wall, no rotation needed, xoffset is 1 width because pivot is bottom right
+        float xOffset = prefabWidth;
+        float yOffset = 0;
+        for (int x = 0; x < calcRoomWidth; x++)
+        {
+            for (int y = 0; y < calcRoomHeight; y++)
+            {
+                Vector3 position = new Vector3((x * prefabWidth) + xOffset, (y * prefabHeight) + yOffset, -calcRoomDepth / 2 * prefabWidth); //room depth to the rear is negative
+                GameObject currentPrefab = Instantiate(wallTilePrefab, position, Quaternion.Euler(0, 0, 0), room.transform); //spawns the prefab facing inward
+                currentPrefab.name = $"Prefab_SouthWall{x}_{y}";
+                wallTilePrefab = wallPrefabList[Random.Range(0, wallPrefabList.Count)];
+            }
+        }
+    }
+
+    void EastWall()
+    {
+        //for east wall, rotation of -90 needed on the y, and the z is equal to the width instead of the x
+        float xOffset = -((calcRoomDepth * prefabWidth) / 2) + prefabWidth;
+        float yOffset = 0;
+        for (int x = 0; x < calcRoomDepth; x++)
+        {
+            for (int y = 0; y < calcRoomHeight; y++)
+            {
+                Vector3 position = new Vector3(calcRoomWidth * prefabWidth, (y * prefabHeight) + yOffset, (x * prefabWidth) + xOffset); //room depth to the rear is negative
+                GameObject currentPrefab = Instantiate(wallTilePrefab, position, Quaternion.Euler(0, -90, 0), room.transform); //spawns the prefab facing inward
+                currentPrefab.name = $"Prefab_EastWall{x}_{y}";
+                wallTilePrefab = wallPrefabList[Random.Range(0, wallPrefabList.Count)];
+            }
+        }
+    }
+
+    void WestWall()
+    {
+        //for west wall, rotation of +90 on the y axis, z is same
+        float xOffset = -((calcRoomDepth * prefabWidth) / 2) + prefabWidth;
+        float yOffset = 0;
+        for (int x = 0; x < calcRoomDepth; x++)
+        {
+            for (int y = 0; y < calcRoomHeight; y++)
+            {
+                Vector3 position = new Vector3(0, (y * prefabHeight) + yOffset, -((x * prefabWidth) + xOffset)); //room depth to the rear is negative
+                GameObject currentPrefab = Instantiate(wallTilePrefab, position, Quaternion.Euler(0, 90, 0), room.transform); //spawns the prefab facing inward
+                currentPrefab.name = $"Prefab_WestWall{x}_{y}";
+                wallTilePrefab = wallPrefabList[Random.Range(0, wallPrefabList.Count)];
+            }
+        }
+    }
+    void AdjustRoomSize()
+    {
+        //for now, just rebuild the whole room by destroying and rebuilding. this is clunky, will adjust to a list and dictionary based method that only adds the next or removes the previous layers
+        Destroy(room);
+        CreateRoom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (calcRoomDepth != roomDepth || calcRoomHeight != roomHeight || calcRoomWidth != roomWidth)
+        {
+            //one of the dimensions has changed in the inspector
+            AdjustRoomSize();
+        }
+    }
+}
--- Assets/Scripts/RoomLayoutController.cs
+++ Assets/Scripts/RoomLayoutController.cs
@@ -9,33 +9,30 @@ public class RoomLayoutController : MonoBehaviour
     /// The purpose of this script is to generate room layouts to help create some rooms for our dungeons to be used later in our random layout generator
     /// 1. Determine the type of room needed
     /// 2. make a room based on the type needed
-    /// 3. plane base size is 10 compared to 1 for any other 3d object
+    /// 3. prefab base size is 10 compared to 1 for any other 3d object
     /// </summary>
     /// 
 
     //Room types needed, for now startRoom, endRoom, interestRoom, itemRoom, puzzleRoom, hallwayRoom, hiddenRoom
-    public GameObject plane;
-    public GameObject test;
-    public float roomDepth, roomHeight, roomWidth, planarScaleFactor, tileScaleFactor;
-    private float origRoomDepth, origRoomHeight, origRoomWidth;
+    public GameObject prefab;
+    public int roomDepth, roomHeight, roomWidth;
+    private int origRoomDepth, origRoomHeight, origRoomWidth;
     private GameObject room;
     private Vector3 position = new Vector3(0, 0, 0);
     private Vector3 rotation = new Vector3(0, 0, 0);
     private Vector3 scale = new Vector3(1, 1, 1);
-    private List<string> planeNamesList = new List<string>();
-    private List<Vector3> planePositions = new List<Vector3>();
-    private List<Vector3> planeScales = new List<Vector3>();
-    private List<Vector3> planeRotations = new List<Vector3>();
-    private List<GameObject> planesList = new List<GameObject>();
-
+    private List<string> prefabNamesList = new List<string>();
+    private List<Vector3> prefabPositions = new List<Vector3>();
+    private List<Vector3> prefabScales = new List<Vector3>();
+    private List<Vector3> prefabRotations = new List<Vector3>();
+    private List<GameObject> prefabsList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
         CreateRoom();
         GenerateLists();
-        CreatePlanes();
-        //GenerateWalls();
+        CreatePrefabs();
     }
     // Update is called once per frame
     void Update()
@@ -49,10 +46,6 @@ public class RoomLayoutController : MonoBehaviour
     void CreateRoom()
     {
         Debug.Log("Creating Room");
-        //storing original variables
-
-
-        //Generate a room out of planes, a simple box
         room = new GameObject(); //creates the gameobject that will hold the room
         room.transform.position = new Vector3(0,0,0); //creates room at 0
         room.name = "Room";  //names room
@@ -66,7 +59,7 @@ public class RoomLayoutController : MonoBehaviour
         origRoomHeight = roomHeight;
 
         //create a list to use for naming the planes
-        planeNamesList = new List<string>
+        prefabNamesList = new List<string>
         {
             "floor",
             "northWall",
@@ -75,9 +68,9 @@ public class RoomLayoutController : MonoBehaviour
             "westWall",
             "ceiling"
         };
-        Debug.Log(planeNamesList.Count);
+        Debug.Log(prefabNamesList.Count);
 
-        planePositions = new List<Vector3> //positions should be based on the size of the room
+        prefabPositions = new List<Vector3> //positions should be based on the size of the room
         {
             new Vector3(0, 0, 0), //floor
             new Vector3(0, roomHeight, roomDepth), //northWall
@@ -86,7 +79,7 @@ public class RoomLayoutController : MonoBehaviour
             new Vector3(-roomWidth, roomHeight, 0), //westWall
             new Vector3(0, roomHeight * 2, 0) //ceiling
         };
-        planeScales = new List<Vector3>
+        prefabScales = new List<Vector3>
         {
             new Vector3(roomWidth, 1, roomDepth), //floor
             new Vector3(roomWidth, 1, roomHeight), //northWall
@@ -96,7 +89,7 @@ public class RoomLayoutController : MonoBehaviour
             new Vector3(roomWidth, 1, roomDepth) //ceiling
         };
 
-        planeRotations = new List<Vector3>
+        prefabRotations = new List<Vector3>
         {
             new Vector3(0, 0, 0), //floor
             new Vector3(-90, 0, 0), //northWall
@@ -107,20 +100,20 @@ public class RoomLayoutController : MonoBehaviour
         };
     }
 
-    void CreatePlanes()
+    void CreatePrefabs()
     {
         Debug.Log("Creating Planes");
         //create walls, floor, and ceiling, that will typically be 6 walls for a box, adding in hallways and turns and things can come later
         int count = 0;
-        foreach (string planeName in planeNamesList)
+        foreach (string prefabName in prefabNamesList)
         {
-            position = planePositions[count] * planarScaleFactor;
-            rotation = planeRotations[count];
-            scale = planeScales[count];
-            GameObject currentPlane = Instantiate(plane, position, Quaternion.Euler(rotation), room.transform); //creates the plane, sets its position, rotation, and makes it a child of room
-            planesList.Add(currentPlane);
-            currentPlane.name = planeName;
-            currentPlane.transform.localScale = scale;
+            position = prefabPositions[count];
+            rotation = prefabRotations[count];
+            scale = prefabScales[count];
+            GameObject currentPrefab = Instantiate(prefab, position, Quaternion.Euler(rotation), room.transform); //creates the prefab, sets its position, rotation, and makes it a child of room
+            prefabsList.Add(currentPrefab);
+            currentPrefab.name = prefabName;
+            currentPrefab.transform.localScale = scale;
             count++;
         }
     }
@@ -128,85 +121,21 @@ public class RoomLayoutController : MonoBehaviour
     void RescaleRoom()
     {
         //rescale
-        planePositions = new List<Vector3>();
-        planeRotations = new List<Vector3>();
-        planeScales = new List<Vector3>();
+        prefabPositions = new List<Vector3>();
+        prefabRotations = new List<Vector3>();
+        prefabScales = new List<Vector3>();
         GenerateLists();
 
         int count = 0;
-        foreach (GameObject currentPlane in planesList)
+        foreach (GameObject currentPrefab in prefabsList)
         {
-            position = planePositions[count] * planarScaleFactor;
-            rotation = planeRotations[count];
-            scale = planeScales[count];
-            currentPlane.transform.position = position;
-            currentPlane.transform.eulerAngles = rotation;
-            currentPlane.transform.localScale = scale;
+            position = prefabPositions[count];
+            rotation = prefabRotations[count];
+            scale = prefabScales[count];
+            currentPrefab.transform.position = position;
+            currentPrefab.transform.eulerAngles = rotation;
+            currentPrefab.transform.localScale = scale;
             count++;
         }
     }
-
-    //void GenerateWalls() // Each wall tile is 5 to 1 of a normal 3D object
-    //{
-    //    int count = 0;
-    //    foreach (GameObject currentPlane in planesList)
-    //    {
-    //        string wallName = currentPlane.name;
-
-    //        if (wallName == "floor" || wallName == "ceiling") // Skip if not a wall
-    //        {
-    //            count++;
-    //            continue;
-    //        }
-
-    //        // The pivot of the planes is the center, the pivot of the prefabs/tiles is the bottom right corner
-    //        float planeHalfWidth = currentPlane.transform.localScale.x * 5; // 10 units width
-    //        float planeHalfHeight = currentPlane.transform.localScale.z * 5; // 10 units depth
-
-    //        // Calculate upper left corner of the plane
-    //        Vector3 upperLeftCorner = currentPlane.transform.position + currentPlane.transform.right * (-planeHalfWidth) + currentPlane.transform.forward * planeHalfHeight;
-
-
-    //        //upperleftcorner + offsets will be tiling
-
-    //        Quaternion rotation = Quaternion.LookRotation(-currentPlane.transform.up, Vector3.up); // up equals the correct direction, as planes visible face
-
-    //        GameObject wall = Instantiate(test, upperLeftCorner, rotation);
-
-    //        Debug.Log(wall.GetComponent<Renderer>().bounds.size.x);
-
-
-            //// Zero of prefab is set to zero of plane
-            //wall.transform.rotation = rotation; // Faces the plane's direction
-            //wall.transform.SetParent(currentPlane.transform); // Assigns the child to the parent object
-            //wall.transform.localPosition = Vector3.zero; // Sets the 0 of the object (bottom right) to the 0 of the plane (center)
-
-            //// Adjust wall position - Align its pivot (bottom right) with the plane's upper left
-            //float wallWidth = wall.GetComponent<Renderer>().bounds.size.x; // Assuming this is the width of the wall prefab
-            //float wallHeight = wall.GetComponent<Renderer>().bounds.size.y; // Assuming this is the depth of the wall prefab
-
-            //// Adjust position to move the pivot of the wall to the upper left of the plane
-            //Vector3 adjustedPosition = upperLeftCorner +
-            //                           wall.transform.right * wallWidth +
-            //                           wall.transform.forward * (-wallHeight);
-
-            //adjustedPosition.x = Mathf.RoundToInt(adjustedPosition.x);
-            //adjustedPosition.y = Mathf.RoundToInt(adjustedPosition.y);
-            //adjustedPosition.z = Mathf.RoundToInt(adjustedPosition.z);
-
-            //wall.transform.position = adjustedPosition;
-
-            ////adjust the local position to allign all tiles
-            //adjustedPosition = wall.transform.localPosition;
-            //adjustedPosition.y = 0f;
-            //wall.transform.localPosition = adjustedPosition;
-
-            //adjustedPosition = wall.transform.position;
-
-    //        count++;
-
-    //        //I want to now generate a wall of tiles from the starting tile. 
-    //    }
-    //}
-
 }
